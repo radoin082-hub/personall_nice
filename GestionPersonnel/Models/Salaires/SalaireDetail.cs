4b09ca1a05f23c36def603b135ede3a15449ff22
ghendirmabroukbenalia@gmail.com <ghendirmabroukbenalia@gmail.com>
2024-07-23 16:59:41 -0700

    SalaireDetail

---
+++ GestionPersonnel/Models/Salaires/SalaireDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPersonnel.Models.Salaires
+{
+    public class SalaireDetail
+    {
+        public string NomEmploye { get; set; }
+        public string PrenomEmploye { get; set; }
+        public string NomFonction { get; set; }
+        public decimal Salaire { get; set; }
+        public decimal Primes { get; set; }
+        public decimal Avances { get; set; }
+        public decimal Dettes { get; set; }
+        public decimal SalaireNet { get; set; }
+        public string TypePaiement { get; set; }
+    }
+}
