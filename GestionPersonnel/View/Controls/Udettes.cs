791d435892f1e7be36cc90a08c97acb3ee62b4bf
ghendirmabroukbenalia@gmail.com <ghendirmabroukbenalia@gmail.com>
2024-07-26 17:07:23 -0700

    aa

--- GestionPersonnel/View/Controls/Udettes.cs
+++ GestionPersonnel/View/Controls/Udettes.cs
@@ -33,7 +33,8 @@ namespace GestionPersonnel.View.Controls
             {
                 MessageBox.Show($"An error occurred while loading debt details: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
+        } 
+
 
         private void UpdateDataGridView(List<PaimentsInfo> paimentsInfos)
         {
@@ -46,7 +47,7 @@ namespace GestionPersonnel.View.Controls
 
         private void DettesGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
+           
         }
     }
 }
